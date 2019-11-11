1c36a0c3495033b8fe69b016bb8e25b60f2bab36
michael-a-c <michael.cottow@mail.utoronto.ca>
2019-11-10 21:09:17 -0500

    More progress with chat

---
+++ frontend/src/components/Messenger/appbar.js
@@ -0,0 +1,29 @@
+
+                <CssBaseline />
+                <AppBar position="fixed" >
+                    <Toolbar >
+                        <Grid display="flex" className="messenger-header-main">
+                            <Box className="messenger-header-1" >
+                                <Typography variant="h6" noWrap className="messenger-header-1-text" >
+                                    Chats
+                                </Typography>
+                            </Box>
+                            <Box className="messenger-header-divider">
+                                <Divider
+                                    orientation="vertical"
+                                    
+                                    light
+                                />
+                            </Box>
+                            <Box className="messenger-header-2">
+                                <Typography variant="h6" className="messenger-header-2-text" noWrap >
+                                    Conversation With
+                                </Typography>
+                                <Typography variant="h6" className="messenger-header-name-" noWrap >
+                                    Yeet Yeet
+                                </Typography>
+                            </Box>
+
+                        </Grid>
+                    </Toolbar>
+                </AppBar>
--- frontend/src/components/Messenger/index.jsx
+++ frontend/src/components/Messenger/index.jsx
@@ -6,7 +6,8 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Divider from '@material-ui/core/Divider';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
-
+import MessageHolder from './MessageHolder';
+import MessageComposer from './MessageComposer';
 
 require('./styles.scss')
 
@@ -18,35 +19,8 @@ class Messenger extends React.Component {
     render() {
         return (
             <div>
-
-                <CssBaseline />
-                <AppBar position="fixed" >
-                    <Toolbar >
-                        <Grid display="flex" className="messenger-header-main">
-                            <Box className="messenger-header-1" >
-                                <Typography variant="h6" noWrap className="messenger-header-1-text" >
-                                    Chats
-                                </Typography>
-                            </Box>
-                            <Box className="messenger-header-divider">
-                                <Divider
-                                    orientation="vertical"
-                                    
-                                    light
-                                />
-                            </Box>
-                            <Box className="messenger-header-2">
-                                <Typography variant="h6" className="messenger-header-2-text" noWrap >
-                                    Conversation With
-                                </Typography>
-                                <Typography variant="h6" className="messenger-header-name-" noWrap >
-                                    Nicholas Wong
-                                </Typography>
-                            </Box>
-
-                        </Grid>
-                    </Toolbar>
-                </AppBar>
+                <MessageHolder />
+                <MessageComposer />
             </div>
         )
     }
