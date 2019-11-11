9fdedcec56005a96a1f5ad7fa70e588a8186496e
AndrewRFung <Andrewr.fung@mail.utoronto.ca>
2019-11-10 21:41:10 -0500

    Fixed view contacts bug where data does not load to next page

--- frontend/src/components/ViewContacts/ContactsList.jsx
+++ frontend/src/components/ViewContacts/ContactsList.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import Contact from "./Contact";
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -90,6 +87,7 @@ const useStyles2 = makeStyles(theme => ({
     },
     tableWrapper: {
         overflowX: 'auto',
+        overflow: 'auto'
     },
 }));
 
@@ -105,7 +103,8 @@ export default function ContactsList() {
     };
 
     const handleChangeRowsPerPage = event => {
-        setRowsPerPage(parseInt(event.target.value, 10));
+        // setRowsPerPage(parseInt(event.target.value, 10));
+        setRowsPerPage(+event.target.value);
         setPage(0);
     };
 
@@ -136,10 +135,11 @@ export default function ContactsList() {
                     </TableHead>
                     <TableBody >
                         {contacts.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row =>
-                                <Contact id={row.id}
-                                        username={row.username}
-                                         description={row.description}
-                                         email={row.email}/>
+                            <TableRow key={row.username} className="contact" tabIndex={-1}>
+                                    <TableCell>{row.username}</TableCell>
+                                    <TableCell colSpan="4">{row.description}</TableCell>
+                                    <TableCell >{row.email}</TableCell>
+                            </TableRow>
                         )}
                     </TableBody>
                 </Table>
@@ -167,132 +167,3 @@ export default function ContactsList() {
         </Paper>
     );
 }
-
-// class ContactsList extends React.Component {
-//     constructor(props) {
-//         super(props);
-//
-//         this.state = {
-//             page: 0,
-//             rowsPerPage: 10,
-//             contacts: props.contacts
-//         };
-//
-//         this.setPage = this.setPage.bind(this);
-//         this.setRowsPerPage = this.setRowsPerPage.bind(this);
-//     }
-//
-//
-//
-//     setPage(selection) {
-//         this.setState({
-//             page: selection,
-//         });
-//         // this.props.onFetchPage(this.props.status, selection, this.state.pageSize);
-//     }
-//
-//     setRowsPerPage(selection) {
-//         this.setState({
-//             rowsPerPage: selection,
-//         });
-//     }
-//
-//     handleChangePage = (event, newPage) => {
-//         this.setPage(newPage);
-//     };
-//
-//     handleChangeRowsPerPage = event => {
-//         this.setRowsPerPage(+event.target.value);
-//         this.setPage(0);
-//     };
-//
-//
-//     createData(id, username, description, email) {
-//         return {id, username, description, email};
-//     };
-//     // const rows = [
-//     //     this.createData(1,"Carla99", "A super nice person", "carla@hotamil.com"),
-//     //     this.createData(1,"Carla99", "A super nice person", "carla@hotamil.com"),
-//     //     this.createData(1,"Carla99", "A super nice person", "carla@hotamil.com"),
-//     //     this.createData(1,"Carla99", "A super nice person", "carla@hotamil.com"),
-//     //     this.createData(1,"Carla99", "A super nice person", "carla@hotamil.com"),
-//     //     this.createData(1,"Carla99", "A super nice person", "carla@hotamil.com"),
-//     //     this.createData(1,"Carla99", "A super nice person", "carla@hotamil.com"),
-//     //     this.createData(1,"Carla99", "A super nice person", "carla@hotamil.com"),
-//     //     this.createData(1,"Carla99", "A super nice person", "carla@hotamil.com"),
-//     //     this.createData(1,"Carla99", "A super nice person", "carla@hotamil.com"),
-//     //     this.createData(1,"last", "A super nice person", "carla@hotamil.com")
-//     // ];
-//
-//
-//     render() {
-//         const { classes } = this.props;
-//         const contacts = [
-//             {"id": 1, "username": "Carla99", "description": "A super nice person", "email": "carla@hotamil.com"},
-//             {"id": 2,"username": "Pal", "description": "Likes to league of legends!", "email": "bobmom@gmail.com"},
-//             {"id": 3,"username": "abbas22", "description": "Worked at NVIDIA", "email": "abbas@gmail.com"},
-//             {"id": 4,"username": "bob1", "description": "amazing", "email": "bob1@hotamil.com"},
-//             {"id": 5,"username": "bob2", "description": "super", "email": "bob2@gmail.com"},
-//             {"id": 6,"username": "bob3", "description": "nice", "email": "bob3@gmail.com"},
-//             {"id": 7,"username": "bob4", "description": "wow", "email": "bob4@hotamil.com"},
-//             {"id": 8,"username": "bob5", "description": "I like food", "email": "bob5@gmail.com"},
-//             {"id": 9,"username": "niceguy24", "description": "lol", "email": "reallynice@gmail.com"},
-//             {"id": 10,"username": "hotturbanGG", "description": "Government worker", "email": "simranpreet@gmail.com"},
-//             {"id": 10,"username": "coolguy16", "description": "\"Why so serious?\"", "email": "coolguy@gmail.com"},
-//         ];
-//
-//
-//
-//         const listItems = contacts.map(d => <Contact id={d.id} username={d.username}
-//                                                                 description={d.description}
-//                                                                 email={d.email}/>);
-//
-//         return (
-//             <Paper className={classes.root}>
-//                 <div className={classes.tableWrapper}>
-//                     {/*<Title>Contact List</Title>*/}
-//                     <Table size="medium" stickyHeader aria-label="sticky table">
-//                         <TableHead>
-//                             <TableRow >
-//                                 <TableCell >{UserName}</TableCell>
-//                                 <TableCell colSpan="4">{Description}</TableCell>
-//                                 <TableCell>{Email}</TableCell>
-//                             </TableRow>
-//                         </TableHead>
-//                         <TableBody >
-//                             {contacts.slice(this.state.page * this.state.rowsPerPage,
-//                                 this.state.page * this.state.rowsPerPage + this.state.rowsPerPage).map(row => {
-//                                 return (
-//                                     <Contact id={row.id}
-//                                             username={row.username}
-//                                              description={row.description}
-//                                              email={row.email}/>
-//                                 );
-//                             })}
-//                             {/*{listItems}*/}
-//                         </TableBody>
-//                     </Table>
-//                 </div>
-//                 <TablePagination
-//                     rowsPerPageOptions={[10, 25, 100]}
-//                     component="div"
-//                     count={contacts.length}
-//                     rowsPerPage={this.state.rowsPerPage}
-//                     page={this.state.page}
-//                     backIconButtonProps={{
-//                         'aria-label': 'previous page',
-//                     }}
-//                     nextIconButtonProps={{
-//                         'aria-label': 'next page',
-//                     }}
-//                     onChangePage={this.handleChangePage}
-//                     onChangeRowsPerPage={this.handleChangeRowsPerPage}
-//                     ActionsComponent={TablePaginationActions}
-//                 />
-//             </Paper>
-//             // </React.Fragment>
-//         );
-//     }
-// }
-
-// export default ContactsList;
