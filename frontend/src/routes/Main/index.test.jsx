83e5f129e9c7109caeadd06dbdbc6dab16f8b300
Anandha Padmanaban Prasad <padmanaban.prasad@utoronto.ca>
2019-11-14 12:03:41 -0500

    CLOUD-53 - Modified tests and corrected the project name

--- frontend/src/routes/Main/index.test.jsx
+++ frontend/src/routes/Main/index.test.jsx
@@ -1,11 +1,14 @@
 // Link.react.test.js
 import React from 'react';
-import Home from './Home';
+import Root from './';
+import { BrowserRouter as Router } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 
 test('Link changes the class when hovered', () => {
   const component = renderer.create(
-    <Home />,
+    <Router>
+      <Root />
+    </Router>
   );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
